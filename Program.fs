4d9f2c15b017973123775a6edc95df74e7f1780f
Pekka Tuukkanen <petuukka@gmail.com>
2024-01-08 14:19:10 +0100

    Day25

--- Program.fs
+++ Program.fs
@@ -5,7 +5,7 @@ open System.IO
 module Program =
     [<EntryPoint>]
     let main args =
-        let input = @"c:\users\pektuu\projects\AoC2023\examples\day25" |> File.ReadLines
-        // let input = args[0] |> File.ReadLines
+        // let input = @"c:\users\pektuu\projects\AoC2023\examples\day25" |> File.ReadLines
+        let input = args[0] |> File.ReadLines
         printf "%0A\n" (Day25.part1 input)
         0
